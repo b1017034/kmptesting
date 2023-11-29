34d7d80288b74bf4d3fab35d03cd473be4cb2c84
asarchy <asahi-nakamura@freee.co.jp>
2023-11-29 17:49:55 +0900

    add Todo

--- shared/src/commonMain/kotlin/com/example/kmp_testing/model/Todo.kt
+++ shared/src/commonMain/kotlin/com/example/kmp_testing/model/Todo.kt
@@ -1,5 +1,16 @@
 package com.example.kmp_testing.model
 
 data class Todo(
-  val title: String
+  val title: String,
+  val content: String,
+  var status: TodoStatus
 )
+
+class Todo2(
+  val title: String,
+  val content: String,
+  var status: TodoStatus
+)
+enum class TodoStatus {
+  Todo, Doing, Done
+}
